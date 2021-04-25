6fcad820581a6d562e6ce6d82b6c83f460660d8a
garryson <vengergo@gmail.com>
2021-04-25 11:24:07 +0300

    npm,express initialize and first part done

---
+++ index.js
@@ -0,0 +1,107 @@
+import express, { response } from 'express'
+import morgan from 'morgan'
+
+const app = express();
+const port = 5000;
+
+app.use(morgan('tiny'));
+
+app.use(express.static('static'));
+
+app.use(express.urlencoded({extended : true}));
+
+
+app.get('/hello', (req,resp) => {
+    resp.send('Hello world!');
+});
+
+function exists_value_at_given_pos(value,array,pos)
+{
+    
+    for(let i = 0; i < array.length; i++)
+    {
+        if(array[i][pos] == value)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+let targyak = []
+//targykod,targyneve,targyevfolyam,kurzusokszama,szeminariumokszama,laborokszama
+app.post('/classinfos', (req,resp) => {
+    //validalas
+    let tmp_targy = []
+    const namePattern = /^[A-Z][a-z0-9]+$/;
+    let targykod = JSON.stringify(req.body.targykod);
+    targykod = targykod.replace(/"([^"]+(?="))"/g, '$1');
+    targykod = parseInt(targykod);
+
+    let targyneve = JSON.stringify(req.body.targyneve);
+    targyneve = targyneve.replace(/"([^"]+(?="))"/g, '$1');
+
+    let targyevfolyam = JSON.stringify(req.body.targyevfolyam);
+    targyevfolyam = targyevfolyam.replace(/"([^"]+(?="))"/g, '$1');
+    targyevfolyam = parseInt(targyevfolyam);
+
+    let kurzusokszama = JSON.stringify(req.body.kurzusokszama);
+    kurzusokszama = kurzusokszama.replace(/"([^"]+(?="))"/g, '$1');
+    kurzusokszama = parseInt(kurzusokszama);
+
+    let szeminariumokszama = JSON.stringify(req.body.szeminariumokszama);
+    szeminariumokszama = szeminariumokszama.replace(/"([^"]+(?="))"/g, '$1');
+    szeminariumokszama = parseInt(szeminariumokszama);
+
+    let laborokszama = JSON.stringify(req.body.laborokszama);
+    laborokszama = laborokszama.replace(/"([^"]+(?="))"/g, '$1');
+    laborokszama = parseInt(laborokszama);
+
+
+    if(targykod <= 0 || exists_value_at_given_pos(targykod,targyak,0))
+    {
+        resp.status(400).send('Hibas targykod, vagy mar letezik!');
+    }    
+    else if(! targyneve.match(namePattern))
+    {
+        resp.status(400).send('Hibas targynev!');
+    }    
+    else if(targyevfolyam < 1 || targyevfolyam > 3)
+    {
+        resp.status(400).send('Hibas evfolyam!');
+    }  
+    else if(kurzusokszama < 7)
+    {
+        resp.status(400).send('Kurzusok szama nem megfelelo!');
+    }
+    else if(szeminariumokszama < 7)
+    {
+        resp.status(400).send('Szeminariumok szama nem megfelelo!');
+    }
+    else if(laborokszama < 7)    
+    {
+        resp.status(400).send('Laborok szama nem megfelelo!');
+    }
+    else 
+    {
+        tmp_targy.push(targykod);
+        tmp_targy.push(targyneve);
+        tmp_targy.push(targyevfolyam);
+        tmp_targy.push(kurzusokszama);
+        tmp_targy.push(szeminariumokszama);
+        tmp_targy.push(laborokszama);
+
+        targyak.push(tmp_targy);
+        resp.redirect('/');
+        //console.log(targyak);
+    }
+    
+
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
