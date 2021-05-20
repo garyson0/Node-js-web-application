738febd9a4dc436a811439cf6d59448c79da2c4b
garryson <vengergo@gmail.com>
2021-05-21 00:32:23 +0300

    parallel promises

--- routes/targyak.js
+++ routes/targyak.js
@@ -9,9 +9,14 @@ router.use(express.urlencoded({ extended: true }));
 router.get('/:id', async (req, resp) => {
   const error = '';
   const targyId = { targykod: req.params.id };
-  const targyAdatai = await db.getTantargyInfosById(targyId);
-  const targyTagjai = await db.getMembersOfTantargy(targyId);
-  resp.render('targyadatai', { error, targyAdatai, targyTagjai });
+  // const targyAdatai = await db.getTantargyInfosById(targyId);
+  // const targyTagjai = await db.getMembersOfTantargy(targyId);
+  const [targyAdatai, targyTagjai, targyFilek] = await Promise.all(
+    [db.getTantargyInfosById(targyId), db.getMembersOfTantargy(targyId), db.getTargyFilek(targyId)],
+  );
+  resp.render('targyadatai', {
+    error, targyAdatai, targyTagjai, targyFilek,
+  });
 });
 
 export default router;
