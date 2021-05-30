18545e8975639da9bd7ba0e6af0cf1fade3bdfbb
garryson <vengergo@gmail.com>
2021-05-30 22:48:44 +0300

    pipeline

--- auth/middleware.js
+++ auth/middleware.js
@@ -5,6 +5,8 @@ export function decodeToken(req, resp, next) {
   if (req.cookies.token) {
     try {
       const tokenObject = jwt.verify(req.cookies.token, secret);
+      // szeminarimuon mukodott ugyanilyen formaban, ezert disableltem, de probalom javitani
+      // eslint-disable-next-line no-param-reassign
       resp.locals.tokenObject = tokenObject;
     } catch (err) {
       resp.clearCookie('token');
